1eede706515c6aa26f321739feb35c9e3af245a8
Guillaume <guillaume.escarieux@free.fr>
2014-02-11 10:34:48 +0100

    Ajout de l'archi du programme, création des WS
    Ajout packages DAL*2, UI, BL, création 4 WS

---
+++ src/ProjetReservations/WSVolConsultation/Service1.asmx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Services;
+
+namespace WSVolConsultation
+{
+    /// <summary>
+    /// Description résumée de Service1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    public class Service1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
