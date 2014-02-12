5f49af2344bc3a5e10ac277bf1667d3f7679481c
Guillaume <guillaume.escarieux@free.fr>
2014-02-12 09:43:56 +0100

    Ajout clsConsulteHotel.dll
    dll pour voir les hotels en fct d'un aeroport

--- src/ProjetReservations/libConsulteHotel/clsConsulteHotel.cs
+++ src/ProjetReservations/libConsulteHotel/clsConsulteHotel.cs
@@ -4,33 +4,42 @@ using System.Text;
 using System.Data;
 using System.Data.SqlClient;
 using System.EnterpriseServices;
+using HotelModel;
 
 namespace libConsulteHotel
 {
-    public class clsConsulteHotel
+    public class clsConsulteHotel: ServicedComponent
     {
         public clsConsulteHotel()
         {
         }
 
         [AutoComplete]
-        public int getHotels(string aeroport_proche)
+        public List<clsHotel> getHotels(string aeroport_proche)
         {
-            SqlDataReader reader;
-            SqlConnection MyC = new SqlConnection();
-            MyC.ConnectionString = "Data Source=Environment.MachineName;Initial Catalog=PROJET_DATA;Integrated Security = true";
-            MyC.Open();
+            SqlConnection myC = new SqlConnection();
+            myC.ConnectionString = "Data Source=Environment.MachineName;Initial Catalog=PROJET_DATA;Integrated Security = true";
+            myC.Open();
 
-            SqlCommand MyCom = new SqlCommand("dbo.sp_getHotel", MyC);
-            MyCom.CommandType = CommandType.StoredProcedure;
+            SqlCommand myCom = new SqlCommand("dbo.sp_getHotel", myC);
+            myCom.CommandType = CommandType.StoredProcedure;
+            myCom.Parameters["@AEROPORT_ARRIVEE"].Value = aeroport_proche;
 
-            reader = MyCom.ExecuteReader();
-            
-
-            MyCom.Dispose();
-            MyC.Close();
-            return 0;
+             SqlDataReader reader = myCom.ExecuteReader();
+             List<clsHotel> listHotels = new List<clsHotel>();
+             while (reader.Read())
+             {
+                 clsHotel hotel = new clsHotel();
+                 hotel.ID = reader.GetInt32(0);
+                 hotel.NOM = reader.GetString(1);
+                 hotel.ADRESSE = reader.GetString(2);
+                 hotel.AEROPORT_PROCHE = reader.GetString(3);
+                 hotel.TARIF_NUIT = reader.GetInt32(4); ;
+                 listHotels.Add(hotel);
+             }
+            myCom.Dispose();
+            myC.Close();
+            return listHotels;
         }
-
     }
 }
