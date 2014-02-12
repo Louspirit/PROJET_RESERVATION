15d7c0054cdaa3279c65899ad5898e0b30c8ff20
cguyonnet <clement.guyonnet@gmail.com>
2014-02-12 16:50:21 +0100

    correction conflits clsConsulteVol

--- src/ProjetReservations/libConsulteVol/clsConsulteVol.cs
+++ src/ProjetReservations/libConsulteVol/clsConsulteVol.cs
@@ -24,7 +24,6 @@ namespace libReservationHotel
         {
         }
 
-<<<<<<< HEAD
         public DataSet getVols(string aeroport_depart, string aeroport_arrivee, int annee, int mois, int jour)
         {
             //appel de sp_getVols avec les paramètres
@@ -69,42 +68,8 @@ namespace libReservationHotel
             MyCom.Dispose();
             MyC.Close();
             return DS;
-=======
-        [AutoComplete]
-        public void setReservationHotel(int idHotel, DateTime dateArrivee, int duree, int montant, String nomClient, String prenomClient, String adresseClient, String numeroCarte)
-        {
-            SqlConnection myC = new SqlConnection();
-            myC.ConnectionString = "Data Source=" + Environment.MachineName + ";Initial Catalog=PROJET_RESERVATION;Integrated Security = true";
-            myC.Open();
-            // Vérification que le client n'existe pas déjà 
-            clsLibClient myClient = new clsLibClient();
-            clsClient client = myClient.getClient(nomClient, prenomClient, adresseClient);
-            //Si non, on le crée dans la table client
-            if (client.ID == 0)
-            {
-                myClient.addClient(nomClient, prenomClient, adresseClient, numeroCarte);
-                //On récupère l'id
-                client = myClient.getClient(nomClient, prenomClient, adresseClient);
-            }
 
-            // Ensuite on insère la commande comportant l'id du client
-            SqlCommand myCom = new SqlCommand("dbo.sp_addCmdHotel", myC);
-            myCom.CommandType = CommandType.StoredProcedure;
-            myCom.Parameters.Add("@ID_HOTEL", SqlDbType.Int);
-            myCom.Parameters["@ID_HOTEL"].Value = idHotel;
-            myCom.Parameters.Add("@ID_CLIENT", SqlDbType.Int);
-            myCom.Parameters["@ID_CLIENT"].Value = client.ID;
-            myCom.Parameters.Add("@DATE_ARRIVEE", SqlDbType.DateTime);
-            myCom.Parameters["@DATE_ARRIVEE"].Value = dateArrivee;
-            myCom.Parameters.Add("@DUREE", SqlDbType.Int);
-            myCom.Parameters["@DUREE"].Value = duree;
-            myCom.Parameters.Add("@MONTANT", SqlDbType.Float);
-            myCom.Parameters["@MONTANT"].Value = montant;
-
-            myCom.ExecuteNonQuery();
-            myCom.Dispose();
-            myC.Close();
->>>>>>> 8121d3f2a83ac4e68426c88e664090aeef5999bf
+        
         }
     }
 }
